f7f66d8b65f73a871a705f27e7c5c6af0ea091c4
lyndaflower <umurerwalynda@gmail.com>
2019-09-23 09:41:11 +0200

    add index.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,15 @@
+
+import spark.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+    }
 }
